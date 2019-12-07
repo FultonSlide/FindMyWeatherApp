e1a2c5283cb0e543c425117e84b64788fbeb1e72
Will <Wi_llo@hotmail.com>
2019-12-07 13:53:08 +1100

    added basic api call functionality

---
+++ scripts/forecast.js
@@ -0,0 +1,27 @@
+const key = 'rzGrwJVsbd3n5If4Zh3VEL2vUraLriKr';
+
+//Get City Information
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0];
+};
+
+//Get Weather Information
+const getConditions = async (cityKey) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${cityKey}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data;
+};
+
+getCity('Melbourne')
+    .then(data => getConditions(data.Key))
+    .then(data => console.log(data))
+    .catch(err => console.log(err));

e9e37d8f366a545a2c2adb5748ff365e4877afba
Will <Wi_llo@hotmail.com>
2019-12-07 14:57:07 +1100

    basic app complete

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,27 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
 //Updates the UI with the weather information of the city the user has entered
 const updateUI = (data) => {
-    const cityDetails = data.cityDetails;
-    const cityWeather = data.cityWeather;
+
+    //Destructure properties
+    const { cityDetails, cityWeather } = data;
+
+    //Update time img & weather icon
+    let timeSrc = null;
+    const iconSrc = `img/icons/${cityWeather[0].WeatherIcon}.svg`;
+
+    if(cityWeather[0].IsDayTime){
+        timeSrc = "img/day.svg";
+    } else {
+        timeSrc = "img/night.svg";
+    }
+
+    time.setAttribute('src', timeSrc);
+    icon.setAttribute('src', iconSrc);
 
     //Update details template
     details.innerHTML = `
